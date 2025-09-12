36b21b50eb4a313249c1d64e952a0d2817b7499e
MagmaC4 <magmac495@gmail.com>
2025-09-12 00:58:40 -0500

    Added my first React website

---
+++ intro_react_program/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import TestComponent from './TestComponent.jsx'
+
+function App() {
+  return (    
+    <TestComponent/>
+  )
+}
+
+export default App
